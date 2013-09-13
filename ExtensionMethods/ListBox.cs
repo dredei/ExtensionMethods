4cc2ce8d94cb8b57d259b0f824dd66642d2ff572
dredei <xkadidas@gmail.com>
2013-09-13 19:15:54 +0300

    Добавлен метод автоскролла для ListBox.

---
+++ ExtensionMethods/ListBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExtensionMethods
+{
+    public static class ListBoxExtension
+    {
+        public static void AutoScroll( this ListBox lb )
+        {
+            lb.SelectedIndex = lb.Items.Count - 1;
+            lb.SelectedIndex = -1;
+        }
+    }
+}
