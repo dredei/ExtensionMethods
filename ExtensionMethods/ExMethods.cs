3d8bb2b7a276510cbc35bb793348088fe0ce3873
dredei <xkadidas@gmail.com>
2014-01-28 18:25:01 +0200

    1. Методы для строк вынесены в отдельный класс; 2. Добавлен перегруженый метод Contains.

--- ExtensionMethods/ExMethods.cs
+++ ExtensionMethods/ExMethods.cs
@@ -11,27 +11,6 @@ namespace ExtensionMethods
     public static class ExMethods
     {
         /// <summary>
-        /// Форматирует строку
-        /// </summary>
-        /// <param name="format">Строка или переменная</param>
-        /// <param name="args">Аргументы</param>
-        /// <returns>Возвращает отформатированною строку</returns>
-        public static string F( this string format, params object[] args )
-        {
-            return String.Format( format, args );
-        }
-
-        /// <summary>
-        /// Проверяет пустая ли строка
-        /// </summary>
-        /// <param name="str">Строка</param>
-        /// <returns>Возвращает значение, указывающее пустая ли строка</returns>
-        public static bool IsNullOrEmpty( this string str )
-        {
-            return string.IsNullOrEmpty( str );
-        }
-
-        /// <summary>
         /// Реализует сохранение данных коллекции List
         /// </summary>
         /// <param name="list">Коллекция List</param>
@@ -96,16 +75,6 @@ namespace ExtensionMethods
             readable = readable / 1024;
 
             return sign + readable.ToString( "0.## " ) + suffix;
-        }
-
-        /// <summary>
-        /// Заменяет перевод строки в ресурсах
-        /// </summary>
-        /// <param name="str">Любая строка</param>
-        /// <returns>Строка с переносами строк</returns>
-        public static string FixNewLines( this string str )
-        {
-            return str.Replace( @"\n", Environment.NewLine );
         }
     }
 }
---
+++ ExtensionMethods/Strings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Форматирует строку
+        /// </summary>
+        /// <param name="format">Строка или переменная</param>
+        /// <param name="args">Аргументы</param>
+        /// <returns>Возвращает отформатированною строку</returns>
+        public static string F( this string format, params object[] args )
+        {
+            return string.Format( format, args );
+        }
+
+        /// <summary>
+        /// Проверяет пустая ли строка
+        /// </summary>
+        /// <param name="str">Строка</param>
+        /// <returns>Возвращает значение, указывающее пустая ли строка</returns>
+        public static bool IsNullOrEmpty( this string str )
+        {
+            return string.IsNullOrEmpty( str );
+        }
+
+        /// <summary>
+        /// Заменяет перевод строки в ресурсах
+        /// </summary>
+        /// <param name="str">Любая строка</param>
+        /// <returns>Строка с переносами строк</returns>
+        public static string FixNewLines( this string str )
+        {
+            return str.Replace( @"\n", Environment.NewLine );
+        }
+
+        /// <summary>
+        /// Перегруженый метод для string.Contains, позволяющий игнорировать регистр
+        /// </summary>
+        public static bool Contains( this string source, string toCheck, StringComparison comp )
+        {
+            return source.IndexOf( toCheck, comp ) >= 0;
+        }
+    }
+}
