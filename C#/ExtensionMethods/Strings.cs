2b841b90ec5da76310b15be14e079cc1c25c0281
dredei <xkadidas@gmail.com>
2014-03-01 14:24:23 +0200

    Добавлены методы ToFirstUpper и ToIntWithNull

--- C#/ExtensionMethods/Strings.cs
+++ C#/ExtensionMethods/Strings.cs
@@ -1,6 +1,7 @@
 ﻿#region Using
 
 using System;
+using System.Text;
 
 #endregion
 
@@ -45,6 +46,44 @@ namespace ExtensionMethods
         public static bool Contains( this string source, string toCheck, StringComparison comp )
         {
             return source.IndexOf( toCheck, comp ) >= 0;
+        }
+
+        /// <summary>
+        /// Делает первую букву строки заглавной
+        /// </summary>
+        /// <param name="str">Любая строка</param>
+        /// <returns>Строка, у которой первая буква заглавная</returns>
+        public static string ToFirstUpper( this string str )
+        {
+            if ( string.IsNullOrEmpty( str ) )
+            {
+                return "";
+            }
+            var sb = new StringBuilder( str );
+            sb[ 0 ] = char.ToUpper( sb[ 0 ] );
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Переводит строку в число или возвращает null, если это невозможно
+        /// </summary>
+        /// <param name="str">Число в виде строки</param>
+        /// <returns>Число или null</returns>
+        public static int? ToIntWithNull( this string str )
+        {
+            if ( string.IsNullOrEmpty( str ) )
+            {
+                return null;
+            }
+            int num;
+            if ( int.TryParse( str, out num ) )
+            {
+                return num;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
